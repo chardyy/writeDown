2ab4d690681a19df6def5988883685dc40058dce
richard <richard.orofeo@synacy.com>
2015-07-02 12:04:05 +0800

    added SIAF in all stories controller

--- public/app/controllers/allStoriesCtrl.js
+++ public/app/controllers/allStoriesCtrl.js
@@ -1,15 +1,20 @@
-angular.module('allStoriesCtrl', ['storyService'])
+(function(){
+	'use strict';
+	
+	angular.module('allStoriesCtrl', ['storyService'])
 
-.controller('AllStoriesController', function(stories, socketio){
+	.controller('AllStoriesController', function(stories, socketio){
 
 	var vm = this;
 
 	vm.stories = stories.data;
 
 	socketio.on('story', function(data){
-    	vm.stories.push(data);
-    });
-    
-});
+		vm.stories.push(data);
+	});
 
-//end controller
+	});
+
+	//end controller
+
+})();
--- public/app/controllers/profileCtrl.js
+++ public/app/controllers/profileCtrl.js
@@ -7,6 +7,15 @@ angular.module('profileCtrl', ['profileService'])
 	Profile.update()
 		   .success(function(data){
 		   		vm.user = data;
-		   })
-	
+		   });
+
+    vm.updateUser = function(){
+    	vm.message = '';
+    	Profile.update(vm.profileData)
+    		.success(function(data){
+
+    			vm.profileData = '';
+    			vm.message = data.message;
+    		})
+    }
 })
