7bff46f080c3dbea9da77072a9ade5234bcf7f9d
richard <richard.orofeo@synacy.com>
2015-07-06 09:10:24 +0800

    re-added models and routes

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,22 +17,48 @@
         },
 
         jshint: {
+          options: {
+            force: false
+          },
           files: ['Gruntfile.js', 'public/app/**/*.js'] 
         },
 
          watch: {
-          files: ['<%= jshint.files %>'],
+          files: ['Gruntfile.js','<%= jshint.files %>'],
           tasks: ['jshint']
+        },
+
+        notify_hooks: {
+            options: {
+              enabled: true,
+              max_jshint_notifications: 3, // maximum number of notifications from jshint output
+              success: true, // whether successful grunt executions should be notified automatically
+              duration: 3 // the duration of notification in seconds, for `notify-send only
+            }
+        },
+
+        simplemocha: {
+          test: {
+            src: ['tests/**/*.html'],
+            options: {
+              reporter: 'spec',
+              slow: 200,
+              timeout: 1000
+            }
+          },
         }
 
       });
 
+    grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-typescript');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-simple-mocha');
 
     //register all the tasks
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'simplemocha']);
+    grunt.task.run('notify_hooks');
     
   };
 
